786ed5c4e0e12a8090a23004e94c676d3821e02d
Hesam <hesam.khedri2002@gmail.com>
2025-04-17 18:42:54 +0330

    Feat: add example values to update user swagger

--- src/user/dtos/user.update.dto.ts
+++ src/user/dtos/user.update.dto.ts
@@ -11,27 +11,47 @@ export class UserUpdateDto {
   @IsOptional()
   userName: string;
 
-  @ApiProperty({ required: false, description: 'New phone number' })
+  @ApiProperty({
+    required: false,
+    description: 'New phone number',
+    example: '09335910559',
+  })
   @IsString()
   @IsOptional()
   phoneNumber: string;
 
-  @ApiProperty({ required: false, description: 'New password' })
+  @ApiProperty({
+    required: false,
+    description: 'New password',
+    example: 'password',
+  })
   @IsString()
   @IsOptional()
   password: string;
 
-  @ApiProperty({ required: false, description: 'New firstName' })
+  @ApiProperty({
+    required: false,
+    description: 'New firstName',
+    example: 'hesam',
+  })
   @IsString()
   @IsOptional()
   firstName: string;
 
-  @ApiProperty({ required: false, description: 'New lastName' })
+  @ApiProperty({
+    required: false,
+    description: 'New lastName',
+    example: 'khedri',
+  })
   @IsString()
   @IsOptional()
   lastName: string;
 
-  @ApiProperty({ required: false, description: 'New email' })
+  @ApiProperty({
+    required: false,
+    description: 'New email',
+    example: 'hesam@gmail.com',
+  })
   @IsString()
   @IsOptional()
   email: string;
